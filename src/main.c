a595a1669e309b3790e8c5413b006c8d791f93de
dinamicmultimedia <>
2025-06-03 14:57:04 +0100

    glClear added.

--- src/main.c
+++ src/main.c
@@ -19,6 +19,8 @@ int main(void)
 	glfwMakeContextCurrent(window);
 
 	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
